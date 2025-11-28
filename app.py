2e17b18f8a7e7d8db5b0bd34ca551a05f41b4f44
Gabriel <bielpps@gmail.com>
2025-11-28 11:10:57 -0300

    first commit

---
+++ app.py
@@ -0,0 +1,8 @@
+print('Bem vindo a minha tentativa de ChatBot pela Asimov academy!')
+nome = input('Qual o seu nome ? ')
+print(f'Seja muito bem-vindo {nome}!')
+idade = int(input('Quantos anos você tem? '))
+if idade < 18:
+    print('Nossa como você é jovem!')
+else:
+    print('Nunca é tarde para aprender!')
